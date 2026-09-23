496ae6b32f0ebb72a9994cea7965e6545d9b46ab
thearmankhan <armankhan281028@gmail.com>
2026-09-24 01:10:16 +0530

    Update power-of-two.cpp
    additional  method

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -7,3 +7,12 @@ public:
         return pow(2,logValue)==n;
     }
 };
+
+// *Power of 2 ke binary mein sirf ONE 1 hota hai, baaki sab 0 hote hain.*
+// 1 = 2⁰ → binary 1
+// 2 = 2¹ → binary 10
+// 4 = 2² → binary 100
+// 8 = 2³ → binary 1000
+// 16 = 2⁴ → binary 10000
+
+//  logic if (N > 0 && (N & (N - 1)) == 0)
