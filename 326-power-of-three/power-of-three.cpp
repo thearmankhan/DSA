37a3eb7191c6b59220cfbf7edc1ef8459387c634
thearmankhan <armankhan281028@gmail.com>
2026-09-24 01:13:34 +0530

    Update power-of-three.cpp
    Addition method

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -7,3 +7,16 @@ public:
        return pow(3,logValue)==n;
     }
 };
+
+// another best me
+// 32-bit signed integer range mein 3 ki sabse badi power:
+// 3^19 = 1162261467
+// 3^19 % 3^1 = 0
+// 3^19 % 3^2 = 0
+// 3^19 % 3^3 = 0
+// 3^19 % 3^4 = 0
+// ...
+// 3^19 % 3^19 = 0
+// n > 0 && 1162261467 % n == 0
+// true && true
+// true
