7b73305033da1311b05234eb8ec664cbfead5237
thearmankhan <armankhan281028@gmail.com>
2026-09-24 00:54:20 +0530

    Time: 4 ms (46.50%) | Memory: 9.2 MB (9.72%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,12 +1,9 @@
-
 class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n <= 0)
-            return false;
-        while (n % 3 == 0) {
-            n = n / 3;
-        }
-        return n == 1;
+         return false;
+       int logValue=log10(n)/log10(3);
+       return pow(3,logValue)==n;
     }
 };
